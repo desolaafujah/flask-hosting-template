8e06271bef43888a92478a2a4ee562820e58979e
desolaafujah <desolafujah@icloud.com>
2023-07-07 19:25:19 +0000

    added webhook function

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
+import git
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)
@@ -21,3 +22,14 @@ def register():
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
+
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/CHANGE_TO_PYTHON_ANYWHERE_USERNAME/CHANGE_TO_GITHUB_REPO_NAME')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
