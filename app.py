3f70be3086b87b7071f093a8e9bb8f404bcb0a31
desolaafujah <desolafujah@icloud.com>
2023-07-07 19:28:50 +0000

    made correction to webhooks function

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 @app.route("/update_server", methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('/home/CHANGE_TO_PYTHON_ANYWHERE_USERNAME/CHANGE_TO_GITHUB_REPO_NAME')
+        repo = git.Repo('/home/dfujah/flask-hosting-template')
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
